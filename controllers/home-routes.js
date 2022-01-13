db658b615fa50de31859f87bb17c9bd25fc36d54
Anthony Geritano <anthony.geritano@aya.yale.edu>
2022-01-12 19:29:12 -0500

    psduocoded the home-routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,21 +1,26 @@
 const router = require('express').Router();
-const { Gallery, Painting } = require('../models');
+// FOR THE HOMEPAGE, WE WILL REQUIRE ATRIBUTES FOUND ON THE USER, MOOD, AND ENTRY MODELS FILE
+const { Gallery, Painting, Entry } = require('../models');
 // Import the custom middleware
 const withAuth = require('../utils/auth');
 
-// GET all galleries for homepage
+// GET ALL ENTRIES FOR AN INDIVDUAL USER
 router.get('/', async (req, res) => {
   try {
+    // NEED TO CHANGE THIS TO THE NAME OF THE DATABASE FOR THE VARIABLE NAME, AND AWAIT ON ENTRY.FINDALL...THIS WILL BE USED TO POPULATE GRAPH
     const dbGalleryData = await Gallery.findAll({
       include: [
+        // WILL NEED TO CHANGE THE MODEL TO ENTRY
         {
-          model: Painting,
-          attributes: ['filename', 'description'],
+          model: Entry,
+          // MOOD, MOODCOLOR, DATE OF ENTRY FOR THE GRAPH AND sleep, food, activities FOR WHEN A USER HOVERS OVER AN ENTRY DATA POINT, BUT DOESNT CLICK
+          attributes: ['sleep', 'description'],
         },
       ],
     });
-
+// THESE SHOULD ALL BE CHANGED TO ENTRY, THE DB TO THE DATABASE VARIABLE NAMED ABOVE
     const galleries = dbGalleryData.map((gallery) =>
+    // WHAT DOES PLAIN: TRUE DO?
       gallery.get({ plain: true })
     );
 
@@ -29,14 +34,20 @@ router.get('/', async (req, res) => {
   }
 });
 
-// GET one gallery
-// Use the custom middleware before allowing the user to access the gallery
+// GET ONE ENTRY
+// Use the custom middleware before allowing the user to access the INDIVIDUAL ENTRIES
+// WHEN A USER CLICKS ON THE POINT ON THE PLOT, IT BRINGS UP /ENTRY/:ID
 router.get('/gallery/:id', withAuth, async (req, res) => {
   try {
+    //  NEED TO CHANGE THIS TO THE NAME OF THE DATABASE FOR THE VARIABLE NAME AND AWAIT ON ENTRY.FINDBYPRIMARYKEY...
+    // THIS WILL BE USED TO FIND THE FULL INFORMATION ON ANY INDIVIDUAL ENTRY AND POPULATE UNDERNEATH THE GRAPH
+    // PERHAPS HAVE IT AUTOMATICALLY SHOW THE MOST RECENT ENTRY AS A PLACE HOLDER AND CHANGE TO WHICH EVER ONE IS CLICKED ON?
     const dbGalleryData = await Gallery.findByPk(req.params.id, {
       include: [
         {
+          // CHANGE MODEL TO ENTRY
           model: Painting,
+          // CHANGE THE ATTRIBUTES ID, MOOD, SLEEP, FOOD, ACTIVITIES, DESCRIPTION
           attributes: [
             'id',
             'title',
@@ -48,7 +59,7 @@ router.get('/gallery/:id', withAuth, async (req, res) => {
         },
       ],
     });
-
+// THESE SHOULD ALL BE CHANGED TO ENTRY, THE DB TO THE DATABASE VARIABLE NAMED ABOVE
     const gallery = dbGalleryData.get({ plain: true });
     res.render('gallery', { gallery, loggedIn: req.session.loggedIn });
   } catch (err) {
@@ -57,20 +68,45 @@ router.get('/gallery/:id', withAuth, async (req, res) => {
   }
 });
 
-// GET one painting
-// Use the custom middleware before allowing the user to access the painting
-router.get('/painting/:id', withAuth, async (req, res) => {
+
+// CREATE NEW ENTRY
+router.post('FILLINHEREPROPERROUTE', async (req, res) => {
   try {
-    const dbPaintingData = await Painting.findByPk(req.params.id);
+    // NEED TO CHANGE THIS TO THE NAME OF THE DATABASE FOR THE VARIABLE NAME
+    // CHANGE TO AWAIT ENTRY.CREATE
+    const dbUserData = await User.create({
+      // CHANGE THE BELOW TO MOOD, SLEEP, FOOD, ACTIVITIES, DESCRIPTION
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password,
+    });
 
-    const painting = dbPaintingData.get({ plain: true });
+    req.session.save(() => {
+      req.session.loggedIn = true;
 
-    res.render('painting', { painting, loggedIn: req.session.loggedIn });
+      res.status(200).json(dbUserData);
+    });
   } catch (err) {
     console.log(err);
     res.status(500).json(err);
   }
 });
+
+
+// GET one painting
+// // Use the custom middleware before allowing the user to access the painting
+// router.get('/painting/:id', withAuth, async (req, res) => {
+//   try {
+//     const dbPaintingData = await Painting.findByPk(req.params.id);
+
+//     const painting = dbPaintingData.get({ plain: true });
+
+//     res.render('painting', { painting, loggedIn: req.session.loggedIn });
+//   } catch (err) {
+//     console.log(err);
+//     res.status(500).json(err);
+//   }
+// });
 
 router.get('/login', (req, res) => {
   if (req.session.loggedIn) {
