f815cfd0024992ded62ce9f066e913b4c9fd4c1c
a.breezy <ambrose.wilkins@gmail.com>
2022-01-17 17:51:25 -0500

    creating graph template

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -7,7 +7,7 @@ const withAuth = require("../utils/auth");
 
 // GET ALL ENTRIES FOR AN INDIVDUAL USER
 router.get("/", async (req, res) => {
-	console.log("Here I am")
+	console.log("Here I am");
 	try {
 		// NEED TO CHANGE THIS TO THE NAME OF THE DATABASE FOR THE VARIABLE NAME, AND AWAIT ON ENTRY.FINDALL...THIS WILL BE USED TO POPULATE GRAPH
 		const dbMoodData = await Entry.findAll();
@@ -45,32 +45,30 @@ router.get("/login", (req, res) => {
 // WHEN A USER CLICKS ON THE POINT ON THE PLOT, IT BRINGS UP /ENTRY/:ID
 router.get("/entry/:id", withAuth, async (req, res) => {
 	try {
-	  //  NEED TO CHANGE THIS TO THE NAME OF THE DATABASE FOR THE VARIABLE NAME AND AWAIT ON ENTRY.FINDBYPRIMARYKEY...
-	  // THIS WILL BE USED TO FIND THE FULL INFORMATION ON ANY INDIVIDUAL ENTRY AND POPULATE UNDERNEATH THE GRAPH
-	  // PERHAPS HAVE IT AUTOMATICALLY SHOW THE MOST RECENT ENTRY AS A PLACE HOLDER AND CHANGE TO WHICH EVER ONE IS CLICKED ON?
-	  const dbMoodData = await Entry.findByPk(req.params.id, {
-		include: [
-		  {
-			model: Entry,
-			attributes: [
-			  "id",
-			  "username",
-			  "mood",
-			  "sleep",
-			  "food",
-			  "activities",
+		//  NEED TO CHANGE THIS TO THE NAME OF THE DATABASE FOR THE VARIABLE NAME AND AWAIT ON ENTRY.FINDBYPRIMARYKEY...
+		// THIS WILL BE USED TO FIND THE FULL INFORMATION ON ANY INDIVIDUAL ENTRY AND POPULATE UNDERNEATH THE GRAPH
+		// PERHAPS HAVE IT AUTOMATICALLY SHOW THE MOST RECENT ENTRY AS A PLACE HOLDER AND CHANGE TO WHICH EVER ONE IS CLICKED ON?
+		const dbMoodData = await Entry.findByPk(req.params.id, {
+			include: [
+				{
+					model: Entry,
+					attributes: ["id", "username", "mood", "sleep", "food", "activities"],
+				},
 			],
-		  },
-		],
-	  });
-	  const userGraph = dbMoodData.get({ plain: true });
-	  // THIS render needs to render within the user_page, without creating a new page
-	  // PERHAPS THIS IS A HANDLEBARD PARTIAL
-	  res.render("graph_stats", { userGraph, loggedIn: req.session.loggedIn });
+		});
+		const userGraph = dbMoodData.get({ plain: true });
+		// THIS render needs to render within the user_page, without creating a new page
+		// PERHAPS THIS IS A HANDLEBARD PARTIAL
+		res.render("graph_stats", { userGraph, loggedIn: req.session.loggedIn });
 	} catch (err) {
-	  console.log(err);
-	  res.status(500).json(err);
+		console.log(err);
+		res.status(500).json(err);
 	}
-  });
+});
+
+router.get("/graph", function (req, res) {
+	console.log("in graph route");
+	res.render("graph");
+});
 
 module.exports = router;
