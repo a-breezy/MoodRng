9427e874eeae61823ed3ae705d73f7c49ee21db1
Anthony Geritano <anthony.geritano@aya.yale.edu>
2022-01-19 18:14:59 -0500

    removed mood seed, corected seed stuff

--- seeds/index.js
+++ seeds/index.js
@@ -1,5 +1,4 @@
 const seedEntry = require('./entryData');
-const seedMood = require('./moodData');
 const seedUser = require('./userData');
 // Do we need a through table?
 //foreign key explanation?
@@ -7,18 +6,19 @@ const seedUser = require('./userData');
 const sequelize = require('../config/connection');
 
 const seedAll = async () => {
+
   await sequelize.sync({ force: true });
   console.log('\n----- DATABASE SYNCED -----\n');
+  
+  await seedUser();
+  console.log('\n----- USER TABLE SEEDED -----\n');
   
   await seedEntry();
   console.log('\n----- ENTRY TABLE SEEDED -----\n');
 
-  await seedMood();
-  console.log('\n----- MOOD TABLE SEEDED -----\n');
+  // await seedMood();
+  // console.log('\n----- MOOD TABLE SEEDED -----\n');
 
-  await seedUser();
-  console.log('\n----- USER TABLE SEEDED -----\n');
-  
 // what is this doing?
   process.exit(0);
 };
