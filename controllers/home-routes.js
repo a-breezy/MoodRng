2c44ddd6e1d70f437b9f6be07bd7568acc979075
jillsuarez <87825792+jillsuarez@users.noreply.github.com>
2022-01-19 10:10:12 -0500

    Update home-routes.js
    get.Entry findOne route for graph moved to entry routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -47,33 +47,6 @@ router.get("/entry", (req, res) => {
 });
 
 
-// ***SHOULD BE MOVED TO API ROUTES BC OF THE RENDER
-// GET ONE ENTRY
-// Use the custom middleware before allowing the user to access the INDIVIDUAL ENTRIES
-// WHEN A USER CLICKS ON THE POINT ON THE PLOT, IT BRINGS UP /ENTRY/:ID
-router.get("/entry/:id", withAuth, async (req, res) => {
-	try {
-		//  NEED TO CHANGE THIS TO THE NAME OF THE DATABASE FOR THE VARIABLE NAME AND AWAIT ON ENTRY.FINDBYPRIMARYKEY...
-		// THIS WILL BE USED TO FIND THE FULL INFORMATION ON ANY INDIVIDUAL ENTRY AND POPULATE UNDERNEATH THE GRAPH
-		// PERHAPS HAVE IT AUTOMATICALLY SHOW THE MOST RECENT ENTRY AS A PLACE HOLDER AND CHANGE TO WHICH EVER ONE IS CLICKED ON?
-		const dbMoodData = await Entry.findByPk(req.params.id, {
-			include: [
-				{
-					model: Entry,
-					attributes: ["id", "username", "mood", "sleep", "food", "activities"],
-				},
-			],
-		});
-		const userGraph = dbMoodData.get({ plain: true });
-		// THIS render needs to render within the user_page, without creating a new page
-		// PERHAPS THIS IS A HANDLEBARD PARTIAL
-		res.render("graph_stats", { userGraph, loggedIn: req.session.loggedIn });
-	} catch (err) {
-		console.log(err);
-		res.status(500).json(err);
-	}
-});
-
 router.get("/graph", function (req, res) {
 	console.log("in graph route");
 	res.render("graph");
